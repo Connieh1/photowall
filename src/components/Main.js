19c3db48a4e2ff9e5075ee18569f9b19bc0417c9
github username <constance.hyman@mchsi.com>
2020-10-31 20:52:55 -0700

    Partially build add/delete functions

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-
 import Title from "./Title";
 import PhotoWall from "./PhotoWall";
+import AddPhoto from "./AddPhoto";
 
 class Main extends Component {
   constructor() {
@@ -28,6 +28,7 @@ class Main extends Component {
             "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg",
         },
       ],
+      screen: "photos", //photos, addPhoto
     };
   }
 
@@ -38,14 +39,71 @@ class Main extends Component {
     }));
   };
 
+  navigate = () => {
+    this.setState({
+      screen: "addPhoto",
+    });
+  };
+
+  componentDidMount() {
+    //   const posts = SimulateFetchFromDatabase();
+    //   this.setState({
+    //     posts: posts,
+    //   });
+    // console.log("componentDidMount");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // alert('re-render')
+    console.log(prevState.posts);
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div>
-        <Title title={"Photowall"} />
-        <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} />
+        {this.state.screen === "photos" && (
+          <div>
+            <Title title={"Photowall"} />
+            <PhotoWall
+              posts={this.state.posts}
+              onRemovePhoto={this.removePhoto}
+              onNavigate={this.navigate}
+            />
+          </div>
+        )}
+
+        {this.state.screen === "addPhoto" && (
+          <div>
+            <AddPhoto />
+          </div>
+        )}
       </div>
     );
   }
 }
+
+// function SimulateFetchFromDatabase() {
+//   return [
+//     {
+//       id: "0",
+//       description: "beautiful landscape",
+//       imageLink:
+//         "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
+//         "3919321_1443393332_n.jpg",
+//     },
+//     {
+//       id: "1",
+//       description: "Aliens???",
+//       imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg",
+//     },
+//     {
+//       id: "2",
+//       description: "On a vacation!",
+//       imageLink:
+//         "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg",
+//     },
+//   ];
+// }
 
 export default Main;
--- src/components/PhotoWall.js
+++ src/components/PhotoWall.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Photo from "./Photo";
+import PropTypes from "prop-types";
+// anchor tag, href attribute
 
 function Photowall(props) {
   return (
-    <div className="photoGrid">
-      {props.posts.map((post, index) => (
-        <Photo post={post} key={index} onRemovePhoto={props.onRemovePhoto} />
-      ))}
+    <div>
+      <a className="addIcon" onClick={props.onNavigate} href="#AddPhoto"></a>
+      <div className="photoGrid">
+        {props.posts.map((post, index) => (
+          <Photo post={post} key={index} onRemovePhoto={props.onRemovePhoto} />
+        ))}
+      </div>
     </div>
   );
 }
+
+Photowall.propTypes = {
+  posts: PropTypes.array.isRequired,
+  onRemovePhoto: PropTypes.func.isRequired,
+};
 
 export default Photowall;
